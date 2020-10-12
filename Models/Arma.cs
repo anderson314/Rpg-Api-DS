3760cad1291f6676639678d8c5aac2d2b761385c
Anderson Rodrigo <andersonrodrigo03.14@gmail.com>
2020-10-12 18:21:01 -0300

    2° publicação da API - Adicionado o objeto Arma

--- Models/Arma.cs
+++ Models/Arma.cs
@@ -2,6 +2,7 @@ namespace RpgApi.Models
 {
     public class Arma
     {
+        //Não atribuir valor a uma 'Id'.
         public int Id { get; set; }
         public string Nome { get; set; }
         public int Dano { get; set; }
